52e112f8c8998afedfa51dade56598b104a7112a
MMAGES2006 <22170@portalcedi.edu.mx>
2024-04-03 19:48:26 -0600

    14

--- usuario.cpp
+++ usuario.cpp
@@ -43,7 +43,11 @@ void Usuario::agregarAmigo(Usuario *nuevoAmigo)
 void Usuario::crearPublicacion()
 {
     cout << "Nueva publicacion" << endl; 
-    Publicacion pn 
+    Publicacion pn(Usuario *usuario);
+    pn.fesha = 0; 
+    cin >> fesha; 
+
+    
 
     
 
